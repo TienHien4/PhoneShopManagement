8e857d179731b9dcbe20529f056e3ce8f0ea0750
Admin <kurumilop8@gmail.com>
2025-07-01 14:26:15 +0700

    update redis config

--- src/main/java/com/example/quanlybandienthoai/configuratuon/RedisConfig.java
+++ src/main/java/com/example/quanlybandienthoai/configuratuon/RedisConfig.java
@@ -1,23 +1,34 @@
 package com.example.quanlybandienthoai.configuratuon;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
+
 
 @Configuration
 public class RedisConfig {
+        @Bean
+        public LettuceConnectionFactory redisConnectionFactory() {
+                RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+                config.setHostName("maglev.proxy.rlwy.net");
+                config.setPort(27802);
+                config.setUsername("default"); //
+                config.setPassword(RedisPassword.of("usZfplEYJpbHnzwLiZlASZKlxiSOhngF"));
+
+                LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                        .build();
+
+                return new LettuceConnectionFactory(config, clientConfig);
+        }
+
         @Bean
         public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory, ObjectMapper redisObjectMapper) {
                 RedisTemplate<String, Object> template = new RedisTemplate<>();
